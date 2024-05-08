463db80e04e1b0511fc3ee85dce4521fd4080bcf
farleybsd <farley.t.i@hotmail.com>
2024-05-08 13:27:51 -0300

    Composite

--- Projeto.net/Com.Padroes.De.Projeto/Src/Com.Padroes.De.Projeto.Composite/Program.cs
+++ Projeto.net/Com.Padroes.De.Projeto/Src/Com.Padroes.De.Projeto.Composite/Program.cs
@@ -1,5 +1,8 @@
 ﻿
 using Com.Padroes.De.Projeto.Composite;
+/*
+ * "Esta seção apresenta o padrão ​ ​Composite​ ​, cujo objetivo é prover uma solução para objetos que representam um conjunto de objetos, mas que compartilham a mesma abstração deles.
+ */
 
 TrechoSimples ts1 = new TrechoSimples("Sao Paulo","Brasilia",500);
 TrechoSimples ts2 = new TrechoSimples("Brasilia", "Recife", 300);
